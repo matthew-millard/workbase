0e37aa28c7b145675a280022129ffdf197c29539
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-05-24 16:14:26 -0400

    Add database schema file

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE workbase;
+
+USE workbase;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+)
--- server.js
+++ server.js
@@ -13,4 +13,3 @@ const db =mysql.createConnection(
     }
 )
 
-
