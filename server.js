587b7d9549a208bd7b04a5e3f26262ee5f068864
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-05-29 11:42:58 -0400

    Feat: assign manager to new employee

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ const queries = require('./db/queries')
 require('dotenv').config()
 
 // MySQL Connection
-const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: process.env.DB_PASSWORD,
-	database: 'workbase',
-})
+const db = mysql
+	.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: process.env.DB_PASSWORD,
+		database: 'workbase',
+	})
+	.promise()
 
 // Main Menu
-function mainMenu() {
-	inquirer
-		.prompt([
+async function mainMenu() {
+	try {
+		const answers = await inquirer.prompt([
 			{
 				type: 'list',
 				name: 'mainMenu',
@@ -23,76 +25,73 @@ function mainMenu() {
 				choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role'],
 			},
 		])
-		.then(answers => {
-			switch (answers.mainMenu) {
-				case 'View All Departments':
-					viewTable(queries['View All Departments'])
-					break
-				case 'View All Roles':
-					viewTable(queries['View All Roles'])
-					break
-				case 'View All Employees':
-					query = 'SELECT * FROM ??'
-					viewTable(queries['View All Employees'])
-					break
-				case 'Add A Department':
-					addDepartment()
-					break
-				case 'Add A Role':
-					addRole()
-					break
-				case 'Add An Employee':
-					addEmployee()
-					break
-				case 'Update An Employee Role':
-					updateEmployee()
-					break
-			}
-		})
+
+		switch (answers.mainMenu) {
+			case 'View All Departments':
+				await viewTable(queries['View All Departments'])
+				break
+			case 'View All Roles':
+				await viewTable(queries['View All Roles'])
+				break
+			case 'View All Employees':
+				await viewTable(queries['View All Employees'])
+				break
+			case 'Add A Department':
+				await addDepartment()
+				break
+			case 'Add A Role':
+				await addRole()
+				break
+			case 'Add An Employee':
+				await addEmployee()
+				break
+			case 'Update An Employee Role':
+				updateEmployee()
+				break
+		}
+	} catch (error) {
+		console.error(error)
+	}
 }
 
 // Executes a database query on a specified table and outputs the results in a table format in the console
-function viewTable(query) {
-	db.query(query, (err, results) => {
-		if (err) throw err
+async function viewTable(query) {
+	try {
+		const [results] = await db.query(query)
 		console.table(results)
 		mainMenu()
-	})
+	} catch (error) {
+		console.error(error)
+	}
 }
-
 // Prompts user for a new department name, validates input length, and executes a database query to insert the department, handling and logging errors as needed.
-function addDepartment() {
-	inquirer
-		.prompt([
+async function addDepartment() {
+	try {
+		const answers = await inquirer.prompt([
 			{
 				type: 'input',
 				name: 'departmentName',
 				message: 'What is the department name you would like to add?',
 			},
 		])
-		.then(answers => {
-			const depName = answers.departmentName.trim()
-			if (depName.length < 2) {
-				console.error('🚫 Error: Department name must be at least 2 characters.')
-				return mainMenu()
-			}
-			db.query(queries['Add Department'], depName, (err, result) => {
-				if (err) {
-					throw err
-				}
-				console.log(`✅ The ${depName} department was successfully added.`)
-				mainMenu()
-			})
-		})
-		.catch(error => {
-			console.error(error)
-		})
+
+		const depName = answers.departmentName.trim()
+		if (depName.length < 2) {
+			console.error('🚫 Error: Department name must be at least 2 characters.')
+			return mainMenu()
+		}
+		await db.query(queries['Add Department'], depName)
+		console.log(`✅ The ${depName} department was successfully added.`)
+		mainMenu()
+	} catch (error) {
+		console.error(error)
+	}
 }
 
 // Prompts user for new role details, then executes a database query to insert the role, handling and logging errors as needed.
-function addRole() {
-	inquirer
-		.prompt([
+async function addRole() {
+	try {
+		const answers = await inquirer.prompt([
 			{
 				type: 'input',
 				name: 'roleName',
@@ -109,67 +108,62 @@ function addRole() {
 				message: 'What is the department id associated with this role?',
 			},
 		])
-		.then(answers => {
-			const { roleName, salary, departmentId } = answers
-			db.query(queries['Add Role'], [roleName, salary, departmentId], (err, result) => {
-				if (err) {
-					throw err
-				}
-				console.log(`✅ The ${roleName} role was successfully added.`)
-				mainMenu()
-			})
-		})
-		.catch(error => {
-			console.error(error)
-		})
+		const { roleName, salary, departmentId } = answers
+		await db.query(queries['Add Role'], [roleName, salary, departmentId])
+		console.log(`✅ The ${roleName} role was successfully added.`)
+		mainMenu()
+	} catch (error) {
+		console.error(error)
+	}
 }
 
-function addEmployee() {
-	// Query to get all roles
-	db.query('SELECT title FROM role', (err, roles) => {
-		if (err) {
-			throw err
-		}
+async function addEmployee() {
+	try {
+		// Query to get all roles
+		const [roles] = await db.query('SELECT title FROM role')
 		const allRoles = roles.map(role => role.title)
-		inquirer
-			.prompt([
-				{
-					type: 'input',
-					name: 'firstName',
-					message: 'What is the first name of the employee you would like to add?',
-				},
-				{
-					type: 'input',
-					name: 'lastName',
-					message: 'What is the last name of the employee you would like to add?',
-				},
-				{
-					type: 'list',
-					name: 'role',
-					message: 'What is the role of the employee you would like to add?',
-					choices: allRoles,
-				},
-			])
-			.then(answers => {
-				const { firstName, lastName, role } = answers
-				db.query('SELECT id FROM role WHERE title = ?', [role], (err, result) => {
-					if (err) {
-						throw err
-					}
-					const role_Id = result[0].id
+		// Query to get all employees
+		const [employees] = await db.query('SELECT first_name, last_name FROM employee')
+		const allEmployees = employees.map(employee => `${employee.first_name} ${employee.last_name}`)
+		const answers = await inquirer.prompt([
+			{
+				type: 'input',
+				name: 'firstName',
+				message: 'What is the first name of the employee you would like to add?',
+			},
+			{
+				type: 'input',
+				name: 'lastName',
+				message: 'What is the last name of the employee you would like to add?',
+			},
+			{
+				type: 'list',
+				name: 'role',
+				message: 'What is the role of the employee you would like to add?',
+				choices: allRoles,
+			},
+			{
+				type: 'list',
+				name: 'manager',
+				message: 'Who is the manager of the employee you would like to add?',
+				choices: allEmployees,
+			},
+		])
+		const { firstName, lastName, role, manager } = answers
+		const [result] = await db.query('SELECT id FROM role WHERE title = ?', [role])
+		const role_Id = result[0].id
+		const manager_firstName = manager.split(' ')[0]
+		const manager_lastName = manager.split(' ')[1]
+		const [managerResult] = await db.query('SELECT id FROM employee WHERE first_name = ? AND last_name = ?', [manager_firstName, manager_lastName])
+		const manager_id = managerResult[0].id
 
-					// Query to add employee
-					db.query('INSERT INTO employee (first_name, last_name, role_Id) VALUES (?, ?, ?)', [firstName, lastName, role_Id], (err, result) => {
-						if (err) {
-							throw err
-						}
-						console.log(`✅ ${firstName} ${lastName} was successfully added as a new employee.`)
-						mainMenu()
-					})
-				})
-			})
-			.catch(error => console.error(error))
-	})
+		// Query to add employee
+		await db.query('INSERT INTO employee (first_name, last_name, role_Id, manager_id) VALUES (?, ?, ?, ?)', [firstName, lastName, role_Id, manager_id])
+		console.log(`✅ ${firstName} ${lastName} was successfully added as a new employee.`)
+		mainMenu()
+	} catch (error) {
+		console.error(error)
+	}
 }
 
 // Initiate Program
